3af4210a456ebb3fbed31ee5b36fef01edebb4d2
Sam M <emberdesignltd@protonmail.com>
2021-08-12 14:35:15 +0100

    feat(checkbox): Add module, component and export component

---
+++ src/app/checkbox/checkbox/checkbox.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CheckboxComponent {}
