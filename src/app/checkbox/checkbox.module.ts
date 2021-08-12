6b3f07eb4aeb069be302012690d42292a5d219d3
Sam M <emberdesignltd@protonmail.com>
2021-08-12 15:11:24 +0100

    feat(Checkbox): Add title, subtitle modules

--- src/app/checkbox/checkbox.module.ts
+++ src/app/checkbox/checkbox.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SubtitleModule } from './../subtitle/subtitle.module';
+import { TitleModule } from './../title/title.module';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
   declarations: [CheckboxComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TitleModule, SubtitleModule],
   exports: [CheckboxComponent],
 })
 export class CheckboxModule {}
