6c176772796fbaf38d56d64d4f061defe894aa40
Sam M <emberdesignltd@protonmail.com>
2021-08-12 15:30:56 +0100

    feat(Checkbox): Add checkbox functionality and HTML/SCSS markup

--- src/app/checkbox/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox/checkbox.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+export interface Checkbox {
+  checked: boolean;
+  id: string;
+  label: string;
+  name: string;
+  value: string;
+}
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxComponent {}
+export class CheckboxComponent {
+  @Input() checkboxes?: Checkbox[];
+}
