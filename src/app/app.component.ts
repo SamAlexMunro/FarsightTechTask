7fde8d1d68ea62a655826e5936966f9ef4ff6caf
Sam M <emberdesignltd@protonmail.com>
2021-08-12 16:38:49 +0100

    feat(App): Add new component implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Breadcrumb } from './breadcrumbs/breadcrumbs/breadcrumbs.component';
+import { Checkbox } from './checkbox/checkbox/checkbox.component';
+import { CookieData } from './cookie-bar/cookie-bar/cookie-bar.component';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,50 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly checkboxData: Checkbox[] = [
+    {
+      id: 'waste',
+      name: 'waste',
+      value: 'carcasses',
+      label: 'Waste from animal carcasses',
+      checked: false,
+    },
+    {
+      id: 'waste-2',
+      name: 'waste',
+      value: 'carcasses',
+      label: 'Waste from mines or quarries',
+      checked: false,
+    },
+    {
+      id: 'waste-3',
+      name: 'waste',
+      value: 'farm',
+      label: 'Farm or agricultural waste',
+      checked: false,
+    },
+  ];
+
+  readonly breadcrumbs: Breadcrumb[] = [
+    {
+      url: '',
+      label: 'Home',
+    },
+    {
+      url: '',
+      label: 'Passports, travel and living abroad',
+    },
+    {
+      url: '',
+      label: 'Travel abroad',
+    },
+  ];
+
+  readonly cookieData: CookieData = {
+    serviceName: 'GovUK',
+    title: 'Cookies on GovUK',
+    description:
+      'We use some essential cookies to make this service work. We’d also like to use analytics cookies so we can understand how you use the service and make improvements.',
+  };
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BreadcrumbsModule } from './breadcrumbs/breadcrumbs.module';
+import { CheckboxModule } from './checkbox/checkbox.module';
+import { CookieBarModule } from './cookie-bar/cookie-bar.module';
+import { TextareaModule } from './textarea/textarea.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    AppRoutingModule,
+    BreadcrumbsModule,
+    BrowserModule,
+    CheckboxModule,
+    CookieBarModule,
+    TextareaModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
